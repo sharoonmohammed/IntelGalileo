137eaee4d70edfbbd81f46df9eb8df3a9eaf99bd
sharoon65 <sharoon65@gmail.com>
2014-11-25 13:36:24 +0530

    Python script to get mail count

---
+++ pyMailCheck.py
@@ -0,0 +1,28 @@
+import urllib2
+import base64
+from xml.dom.minidom import parse
+
+def gmail_unread_count(user, password):
+    """
+        Takes a Gmail user name and password and returns the unread
+        messages count as an integer.
+    """
+    # Build the authentication string
+    b64auth = base64.encodestring("%s:%s" % (user, password))
+    auth = "Basic " + b64auth
+
+    # Build the request
+    req = urllib2.Request("https://mail.google.com/mail/feed/atom/")
+    req.add_header("Authorization", auth)
+    handle = urllib2.urlopen(req)
+
+    # Build an XML dom tree of the feed
+    dom = parse(handle)
+    handle.close()
+
+    # Get the "fullcount" xml object
+    count_obj = dom.getElementsByTagName("fullcount")[0]
+    # get its text and convert it to an integer
+    return int(count_obj.firstChild.wholeText)
+
+print 'You have',gmail_unread_count('disprojectgalileo@gmail.com', 'intelgalileo'),'unread messages'
